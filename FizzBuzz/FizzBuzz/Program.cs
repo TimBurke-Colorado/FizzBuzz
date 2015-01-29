97ec87707c6decb5eeac1021705e936ef58d9019
SeedPaths-User <lessdismissive@email.com>
2015-01-28 17:25:57 -0700

    completed

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -9,11 +9,32 @@ namespace FizzBuzz
     class Program
     {
         static void Main(string[] args)
-        {
-          
+        { 
         }
         public static string FizzBuzz(int number)
         {
+            if (number % 3 == 0)
+            {
+                if (number % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+            };
+            if (number % 3 == 0)
+            {
+                return "Buzz";
+            };
+            if (number % 5 == 0)
+            {
+                return "Fizz";
+            };
+            if (number % 3 != 0)
+            {
+                if (number % 5 != 0)
+                {
+                    return number.ToString();
+                };
+            }
             return "";
         }
     }
